0825734e2e78dfe91b634f08e7f02314b6a2317f
potados99 <potados99@gmail.com>
2019-12-25 00:10:09 +0900

    New implementation for D.

--- solutions/D.py
+++ solutions/D.py
@@ -1,33 +1,32 @@
-N, M = list(map(lambda x: int(x), input().split(' ')))
-
+N, M = map(lambda x: int(x), input().split(' '))
 neighbers = [[] for x in range(0, N+1)]
 for i in range(0, M):
-    city1, city2 = list(map(lambda x: int(x), input().split(' ')))
+    city1, city2 = map(lambda x: int(x), input().split(' '))
     neighbers[city1].append(city2)
     neighbers[city2].append(city1)
-
 n_destroyed = int(input())
-destroyed = list(map(lambda x: int(x), input().split(' ')))
+destroyed = set(map(lambda x: int(x), input().split(' ')))
 
-bombed = []
-for city in destroyed:
-    neighbers_of_this_city = neighbers[city]
-    neighbers_all_destroyed = False not in [x in destroyed for x in neighbers_of_this_city]
 
-    if neighbers_all_destroyed:
-        bombed.append(city)
+# Get bombed cities.
+cities = set(range(1, N+1))
+not_destroyed = cities - destroyed
 
-should_have_been_destroyed = []
+bombed = set(destroyed) # Assume all destroyed cities are bombed.
+for city in not_destroyed:
+    bombed -= set(neighbers[city]) # Remove not actually bombed cities.
+
+# Check validity.
+should_have_been_destroyed = set()
 for city in bombed:
-    should_have_been_destroyed.extend(neighbers[city] + [city])
+    should_have_been_destroyed |= set(neighbers[city]) | {city}
 
-if False in [x in should_have_been_destroyed for x in destroyed]:
+if len(destroyed - should_have_been_destroyed) != 0:
     bombed.clear()
 
 if len(bombed) == 0:
     print(-1)
 else:
     print(len(bombed))
-    bombed.sort()
-    for city in bombed:
+    for city in sorted(list(bombed)):
         print(city, end=' ')
