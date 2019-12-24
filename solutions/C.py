ca02186097ff24a1887ac822952543c747dc81db
potados99 <potados99@gmail.com>
2019-12-25 03:16:58 +0900

    New solution for C.

--- solutions/C.py
+++ solutions/C.py
@@ -1,33 +1,12 @@
-N, A, B = list(map(lambda x: int(x), input().split(' ')))
+width, n_one, n_two = map(lambda x: int(x), input().split(' '))
+one = sorted(map(int, input().split(' ')), reverse=True)
+two = sorted(map(int, input().split(' ')), reverse=True)
 
-# Sorted ascending.
-two_by_one_beauty = sorted(list(map(lambda x: int(x), input().split(' '))))
-two_by_two_beauty = sorted(list(map(lambda x: int(x), input().split(' '))))
-
-horizontal_size = 0
 beauty = 0
-
-# Make N even.
-if N % 2 == 1:
-    beauty += two_by_one_beauty.pop()
-    N -= 1
-
-# horizontal_size will only increase for amount of 2.
-# If number of 2x1 < 2, ignore it.
-# If number of 2x2 < 1, ignore it.
-# If both? impossible.
-while horizontal_size < N:
-    from_2x1 = two_by_one_beauty[-1] + two_by_one_beauty[-2] if len(two_by_one_beauty) > 1 else 0
-    from_2x2 = two_by_two_beauty[-1] if len(two_by_two_beauty) > 0 else 0
-
-    if from_2x1 > from_2x2:
-        beauty += from_2x1
-        two_by_one_beauty.pop()
-        two_by_one_beauty.pop()
-    else:
-        beauty += from_2x2
-        two_by_two_beauty.pop()
-
-    horizontal_size += 2
+# Iterate for bigger one.
+for len_two in range(n_two+1):
+    len_one = width - (2 * len_two)
+    if not 0 <= len_one <= n_one: continue
+    beauty = max(beauty, sum(one[:len_one]) + sum(two[:len_two]))
 
 print(beauty)
